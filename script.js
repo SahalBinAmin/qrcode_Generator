932476347ed23ac2c00c20ba4b932a64edd70d2f
Sahal Bin Amnin <sahalbinamin.37@gmail.com>
2025-05-23 17:22:23 +0500

    code base Finalised

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let api =
-  " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example";
-
+let imgBox = document.querySelector("#imgBox");
+let qrImage = document.querySelector("#qr-Image");
 let input = document.querySelector("input");
-let val = input.value;
+let btn = document.querySelector("button");
 
-console.log(val);
+function genQRCode() {
+  if (input.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      input.value;
+    imgBox.classList.add("show-img");
+  } else {
+    input.classList.add("error");
+    setTimeout(() => {
+      input.classList.remove("error");
+    }, 1000);
+  }
+}
+
+btn.addEventListener("click", () => {
+  genQRCode();
+});
